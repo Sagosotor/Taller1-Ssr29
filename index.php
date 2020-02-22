8090a373962383d1594d9940d4fc19301267b78c
Sagosotor <61335509+Sagosotor@users.noreply.github.com>
2020-02-21 21:14:02 -0500

    suma,resta,multiplicacion

--- index.php
+++ index.php
@@ -1,4 +1,33 @@
 <?php
 
-echo"Hola medellin"
+echo"Ejercicio Realizado Por Santiago Soto R.";
+echo"<br>Ejercicio # 1 ";
+
+$numero1=50;
+$numero2=50;
+
+echo"<br>Suma<br>";
+
+$suma=$numero1+$numero2;
+
+echo("La suma de numero1 y numero2 es:".$suma."<br><br>");
+
+echo"<br>Resta<br>";
+
+$resta=$numero1-$numero2;
+
+echo("la resta de numero1 y numero2 es:".$resta."<br><br>");
+
+echo"<br>Multiplicacion<br>";
+
+$multiplicacion=$numero1*$numero2;
+
+echo("La multiplicacion de numero1 y numero2 es:".$multiplicacion. "<br><br>");
+
+echo"<br>Division<br>";
+
+$division=$numero1/$numero2;
+
+echo("La division de numero1 y numero2 es:".$division. "<br><br>");
+
 ?>
